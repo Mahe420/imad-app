a66a582f395e529e6053caed6787d725e13bb52c
Mahe420 <30702093+Mahe420@users.noreply.github.com>
2017-09-12 17:19:31 +0530

    Update mydemo.java

--- ui/mydemo.java
+++ ui/mydemo.java
@@ -4,11 +4,11 @@ import javax.servlet.*;
 
 public class mydemo extends HttpServlet
 {
- public void service(HttpServletRequest req,HttpServletResponse res)
+ public void get(HttpServletRequest req,HttpServletResponse res)
  throws ServletException,IOException
  {
   PrintWriter out=res.getWriter();
-  String name=req.getParameter("name).value;
+  String name=req.getParameter("name").value;
    out.println(name);
   }
 out.close();}
